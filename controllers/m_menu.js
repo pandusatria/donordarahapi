d8e2d0563b6263e87d29ec23c8c9c6d5cc759225
pandusatria <pandu.ananda@xsis.co.id>
2018-10-18 18:41:52 +0700

    ObjectId -> ObjectID

--- controllers/m_menu.js
+++ controllers/m_menu.js
@@ -37,7 +37,7 @@ const MenuController = {
             {
                 $match:
                 {
-                    "_id": ObjectId(id_role)
+                    "_id": ObjectID(id_role)
                 }
             },
             {

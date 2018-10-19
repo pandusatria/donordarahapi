fa233bbe921a7a99e1a7afa15ee8466a8585cb1e
pandusatria <pandu.ananda@xsis.co.id>
2018-10-19 10:07:11 +0700

    back to before

--- controllers/m_menu.js
+++ controllers/m_menu.js
@@ -55,9 +55,9 @@ const MenuController = {
                 return next(new Error());
             }
 
-            /*let modelCollection = data.map((entity) => {
+            let modelCollection = data.map((entity) => {
                 return new menuModel(entity);
-            });*/
+            });
 
             Response.send(res, 200, data);
         });
